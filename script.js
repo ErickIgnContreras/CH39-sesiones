861372e8c60e8d86dfe4cb0c4ce43fb2f599c25c
Erick Contreras <casiqbp@gmail.com>
2024-02-19 13:22:57 -0600

    ci mensajes y alertas

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log ("Hola CH38");
+
+//funcion que envie saludo con alerta //
+
+function enviarSaludo(){
+ alert(`Saludos`);
+}
+
+/**
+ * forma correcta de comentar la funcion
+ * Pedir el nombre de la persona usando un prmt()
+ * Mostrar un saludo y el nombre de la persona
+ */
+
+function enviarSaludoAPersona(){
+let message= prompt("Escribe tu nombre");
+    alert(`Saludos amigo ${message }`)
+}
